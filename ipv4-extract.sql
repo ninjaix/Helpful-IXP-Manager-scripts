cc0ed1631202c7a5b0fdf35d39ed416019f30d08
Paul Emmons <76638404+ninjaix@users.noreply.github.com>
2023-01-07 11:50:03 -0700

    Update and rename ipv4-extract.sql to ipv4-extract-new.sql

--- ipv4-extract.sql
+++
@@ -1,9 +0,0 @@
-SELECT `ipv4hostname`,`ipv4canping`,`address`,`name` FROM `ixpmanager`.`vlaninterface` 
-JOIN `ixpmanager`.`ipv4address` 
-ON (`ipv4address`.`id`= `ipv4addressid`) 
-JOIN `ixpmanager`.`vlan` 
-ON  `vlan`.`id`=`vlaninterface`.`vlanid` 
-WHERE `ipv4canping` is true and `address`is Not null
-INTO OUTFILE '/var/lib/mysql-files/ipv4-dns.csv'
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n';
