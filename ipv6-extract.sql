2308ff5b846019387c383709e6662889c543c335
Paul Emmons <76638404+ninjaix@users.noreply.github.com>
2023-01-07 11:52:36 -0700

    Update ipv6-extract.sql

--- ipv6-extract.sql
+++ ipv6-extract.sql
@@ -1,8 +1,15 @@
-SELECT `ipv6hostname`,`ipv6canping`,`address`,`name` FROM `ixpmanager`.`vlaninterface` 
-JOIN `ixpmanager`.`ipv6address` 
-ON (`ipv6address`.`id`= `ipv6addressid`) 
-JOIN `ixpmanager`.`vlan` 
-ON  `vlan`.`id`=`vlaninterface`.`vlanid` where `ipv6canping` is true and `address`is Not null
-INTO OUTFILE '/var/lib/mysql-files/ipv6-dns.csv'
+SELECT 
+    concat(`ixpmanager`.`vlaninterface`.`ipv6hostname`,".ix.",`ixpmanager`.`vlan`.`name`,".Ninja-IX.net"),
+    `ixpmanager`.`vlaninterface`.`ipv4canping`,
+    concat(`ipv6address`.`address`,"/24"),
+    `ixpmanager`.`cust`.`name`,
+    `ixpmanager`.`vlan`.`number`
+FROM     `ixpmanager`.`vlaninterface`
+        JOIN     `ixpmanager`.`ipv6address` ON (`ipv6address`.`id` = `ipv6addressid`)
+        JOIN     `ixpmanager`.`vlan` ON (`vlan`.`id` = `vlaninterface`.`vlanid`)
+        JOIN     `ixpmanager`.`virtualinterface` ON (`virtualinterface`.`id` = `vlaninterface`.`virtualinterfaceid`)
+        JOIN     `ixpmanager`.`cust` ON (`cust`.`id` = `virtualinterface`.`custid`)
+WHERE     `ipv6address`.`address` IS NOT NULL and `vlan`.`private` is false
+    ORDER BY `ipv6address`.`address`;
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
