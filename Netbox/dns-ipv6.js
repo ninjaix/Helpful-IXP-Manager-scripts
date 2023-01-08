480b594d33cfa4216078d3f74baafb3ace545407
Paul Emmons <76638404+ninjaix@users.noreply.github.com>
2023-01-08 10:26:34 -0700

    Update dns-ipv6.js

--- Netbox/dns-ipv6.js
+++ Netbox/dns-ipv6.js
@@ -23,16 +23,17 @@ mycon.connect((err) => {
 });
 
 mycon.query(
-'SELECT concat(`ixpmanager`.`vlaninterface`.`ipv6hostname`,".",`ixpmanager`.`vlan`.`name`,".IX.Ninja-IX.net"),`ixpmanager`.`vlaninterface`.'+
-'`ipv4canping`,concat(`ipv6address`.`address`,"/64"), `ixpmanager`.`cust`.`name`, `ixpmanager`.`vlan`.`number`,`ixpmanager`.`ipv6address`.`updated_at`,'+
-'`ixpmanager`.`cust`.`autsys`, `ixpmanager`.`vlan`.`private`,`ixpmanager`.`vlan`.`name`,`ixpmanager`.`vlaninterface`.`ipv6enabled`,'+
+'SELECT concat(`ixpmanager`.`vlaninterface`.`ipv6hostname`,".",`ixpmanager`.`vlan`.`name`,".IX.Ninja-IX.net"),`ixpmanager`.`vlaninterface`.`ipv4canping`,'+
+'concat(`ipv6address`.`address`,"/64"), `ixpmanager`.`cust`.`name`, `ixpmanager`.`vlan`.`number`,`ixpmanager`.`ipv6address`.`updated_at`,'+
+'`ixpmanager`.`cust`.`autsys`,`ixpmanager`.`vlan`.`private`,`ixpmanager`.`vlan`.`name`,`ixpmanager`.`vlaninterface`.`ipv6enabled`,'+
 '`ixpmanager`.`vlaninterface`.`ipv6monitorrcbgp`,`ixpmanager`.`vlaninterface`.`busyhost`,'+
-'concat("https://portal.ninja-ix.net/customer/overview/",`ixpmanager`.`cust`.`id`'+  
+'concat("https://portal.ninja-ix.net/customer/overview/",`ixpmanager`.`cust`.`id`)'+
 'FROM `ixpmanager`.`vlaninterface`'+
 'JOIN `ixpmanager`.`ipv6address` ON (`ipv6address`.`id` = `ipv6addressid`)'+
 'JOIN `ixpmanager`.`vlan` ON (`vlan`.`id` = `vlaninterface`.`vlanid`)'+
 'JOIN `ixpmanager`.`virtualinterface` ON (`virtualinterface`.`id` = `vlaninterface`.`virtualinterfaceid`)'+
 'JOIN `ixpmanager`.`cust` ON (`cust`.`id` = `virtualinterface`.`custid`)'+
+'JOIN `ixpmanager`.`company_registration_detail` ON (`company_registration_detail`.`id` = `cust`.`company_registered_detail_id`)'+
 'WHERE `ipv6address`.`address` IS NOT NULL and (`vlan`.`private` is FALSE)'+
 'ORDER BY `ipv6address`.`address`',
 function (err, data, fields) {
