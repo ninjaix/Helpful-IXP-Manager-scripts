93582f260341f0985ef50e16cbf377b1fe4ee89a
Paul Emmons <76638404+ninjaix@users.noreply.github.com>
2023-01-08 12:55:22 -0700

    Update dns-ipv6.js

--- Netbox/dns-ipv6.js
+++ Netbox/dns-ipv6.js
@@ -6,7 +6,6 @@ var mysql = require('mysql');
 const fastcsv = require("fast-csv");
 const fs = require("fs");
 const ws = fs.createWriteStream("output/ipv6.csv");
-
 // create a connection variable with the required details
 var mycon = mysql.createConnection({
   host:         process.env.MYSQLHOST,
@@ -23,11 +22,19 @@ mycon.connect((err) => {
 });
 
 mycon.query(
-'SELECT concat(`ixpmanager`.`vlaninterface`.`ipv6hostname`,".",`ixpmanager`.`vlan`.`name`,".IX.Ninja-IX.net") AS `dns-name`,`ixpmanager`.`vlaninterface`.`ipv6canping`,'+
-'concat(`ipv6address`.`address`,"/64")AS `ipaddress` , `ixpmanager`.`cust`.`name`, `ixpmanager`.`company_registration_detail`.`registeredName`,'+
-'`ixpmanager`.`vlan`.`number`,`ixpmanager`.`ipv6address`.`updated_at`,'+
-'`ixpmanager`.`cust`.`autsys`,`ixpmanager`.`vlan`.`private`,`ixpmanager`.`vlan`.`name`,`ixpmanager`.`vlaninterface`.`ipv6enabled`,'+
-'`ixpmanager`.`vlaninterface`.`ipv6monitorrcbgp`,`ixpmanager`.`vlaninterface`.`busyhost`,'+
+'SELECT concat(`ixpmanager`.`vlaninterface`.`ipv6hostname`,".",`ixpmanager`.`vlan`.`name`,".IX.Ninja-IX.net") AS `dns-name`,'+
+'`ixpmanager`.`vlaninterface`.`ipv6canping` AS `canping`,'+
+'concat(`ipv6address`.`address`,"/64") AS `ipaddress`,'+
+'`ixpmanager`.`cust`.`name` AS `network-name`,'+
+'`ixpmanager`.`company_registration_detail`.`registeredName`,'+
+'`ixpmanager`.`vlan`.`number`,'+
+'`ixpmanager`.`ipv6address`.`updated_at` AS `updated_at`,'+
+'`ixpmanager`.`cust`.`autsys`,'+
+'`ixpmanager`.`vlan`.`private`,'+
+'`ixpmanager`.`vlan`.`name` AS `vlan-name`,'+
+'`ixpmanager`.`vlaninterface`.`ipv6enabled` AS `enabled`,'+
+'`ixpmanager`.`vlaninterface`.`ipv6monitorrcbgp` AS `monitorcbgp`,'+
+'`ixpmanager`.`vlaninterface`.`busyhost`,'+
 'concat("https://portal.ninja-ix.net/customer/overview/",`ixpmanager`.`cust`.`id`) AS `IXPM-Link`'+
 'FROM `ixpmanager`.`vlaninterface`'+
 'JOIN `ixpmanager`.`ipv6address` ON (`ipv6address`.`id` = `ipv6addressid`)'+
@@ -35,7 +42,7 @@ mycon.query(
 'JOIN `ixpmanager`.`virtualinterface` ON (`virtualinterface`.`id` = `vlaninterface`.`virtualinterfaceid`)'+
 'JOIN `ixpmanager`.`cust` ON (`cust`.`id` = `virtualinterface`.`custid`)'+
 'JOIN `ixpmanager`.`company_registration_detail` ON (`company_registration_detail`.`id` = `cust`.`company_registered_detail_id`)'+
-'WHERE `ipv6address`.`address` IS NOT NULL and (`vlan`.`private` is FALSE)'+
+'WHERE (`ipv6address`.`address` IS NOT NULL)'+
 'ORDER BY `ipv6address`.`address`',
 function (err, data, fields) {
     if (err) throw err;
